ecce57cf9b7afbd2579042c59c66f9dd3af140d1
viktorashi <33178074+viktorashi@users.noreply.github.com>
2024-07-02 15:13:04 +0300

    TOT LABU 2

---
+++ backend/tests/conftest.py
@@ -0,0 +1,24 @@
+import pytest
+from app import create_app
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
--- backend/tests/test_routes.py
+++ backend/tests/test_routes.py
@@ -1,56 +1,75 @@
-import pytest
 import json
-import app
+from app import create_app
 
-@pytest.fixture
-def client():
-    app.config['TESTING'] = True
-    with app.test_client() as client:
-        yield client
+items = json.load(open('periodic-table.json','r'))
 
-@pytest.fixture(autouse=True)
-def inject_config(pytestconfig):
-    # Example: Accessing a pytest option named 'debug'
-    debug_mode = pytestconfig.getoption('debug')
-    # You can also access other configuration options here if needed
 
-def test_create(client):
-    # Test functions as previously defined...
-    data = {'id': '1', 'name': 'Test User'}
-    response = client.post('/create', json=data)
+# def test_create(client):
+#     # Test functions as previously defined...
+#     data = {'id': '1', 'name': 'Test User'}
+#     response = client.post('/create', json=data)
+#     assert response.status_code == 201
+
+
+
+def test_get_items(client):
+    response = client.get('/items')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data == items
+
+def test_get_item(client):
+    response = client.get('/details/1')
+    assert response.status_code == 200
+    data = json.loads(response.data)
+    assert data['number'] == 1
+
+def test_update_item(client):
+    data= {'name': 'mortii masii', 'description': 'nuj', 'category': 'diatomic nonmetal', 'appearance': 'colorless gas', 'discovered_by': 'Henry Cavendish', 'named_by': 'Antoine Lavoisier', 'phase': 'Gas', 'bohr_model_image': 'https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png', 'summary': 'Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the Universe, constituting roughly 75% of all baryonic mass.'}
+
+    #try updateing an id that doesnt exist
+    response = client.put('/items/999', json=data)
+    assert response.status_code == 404
+
+    response = client.put('/items/1', json=data)
+    assert response.status_code == 200
+
+    response = client.get('/details/1')
+    data = json.loads(response.data)
+    assert data['name'] == 'mortii masii'
+
+    #try updating with invalid data 
+    data = {'sa moara':'franta'}
+
+    response = client.put('/items/1', json=data)
+    assert response.status_code == 400
+
+
+
+def test_create_item(client):
+    data = {'name': 'mortii masii', 'description': 'nuj', 'category': 'diatomic nonmetal', 'appearance': 'colorless gas', 'discovered_by': 'Henry Cavendish', 'named_by': 'Antoine Lavoisier', 'phase': 'Gas', 'bohr_model_image': 'https://storage.googleapis.com/search-ar-edu/periodic-table/element_001_hydrogen/element_001_hydrogen_srp_th.png', 'summary': 'Hydrogen is a chemical element with chemical symbol H and atomic number 1. With an atomic weight of 1.00794 u, hydrogen is the lightest element on the periodic table. Its monatomic form (H) is the most abundant chemical substance in the Universe, constituting roughly 75% of all baryonic mass.'}
+    response = client.post('/items', json=data)
     assert response.status_code == 201
 
-def test_read(client):
-    # Test functions as previously defined...
-    response = client.get('/read/1')
-    assert response.status_code == 200
+    #120 ca e fix ultimu dupa ala
+    response = client.get('/details/120')
     data = json.loads(response.data)
-    assert data['id'] == '1'
+    assert data['name'] == 'mortii masii'
 
-    response = client.get('/read/999')
+    #try submitting invalid data
+    data = {'sa moara':'franta'}
+
+    response = client.post('/items', json=data)
+
+    assert response.status_code == 400
+
+def test_delete_item(client):
+    response = client.delete('/items/1')
+    assert response.status_code == 204
+
+    response = client.get('/details/1')
     assert response.status_code == 404
 
-def test_update(client):
-    # Test functions as previously defined...
-    data = {'name': 'Updated User'}
-    response = client.put('/update/1', json=data)
-    assert response.status_code == 200
-
-    response = client.get('/read/1')
-    data = json.loads(response.data)
-    assert data['name'] == 'Updated User'
-
-    data = {'name': 'Updated User'}
-    response = client.put('/update/999', json=data)
-    assert response.status_code == 404
-
-def test_delete(client):
-    # Test functions as previously defined...
-    response = client.delete('/delete/1')
-    assert response.status_code == 200
-
-    response = client.get('/read/1')
-    assert response.status_code == 404
-
-    response = client.delete('/delete/999')
+    response = client.delete('/items/999')
+    print(response.data)
     assert response.status_code == 404
